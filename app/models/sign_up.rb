d1b83da39320113e4cb96d3708fc1aa5089aef12
Raghu Betina <raghu@firstdraft.com>
2018-06-18 06:56:06 +0000

    Direct association between users and sign_ups

--- app/models/sign_up.rb
+++ app/models/sign_up.rb
@@ -1,6 +1,9 @@
 class SignUp < ApplicationRecord
   # Direct associations
 
+  belongs_to :kwestee,
+             :class_name => "User"
+
   # Indirect associations
 
   # Validations
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_one    :sign_up,
+             :foreign_key => "kwestee_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
