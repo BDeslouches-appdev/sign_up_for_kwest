4d30aca126be2ad29de4991328783e4e73b63099
Raghu Betina <raghu@firstdraft.com>
2018-06-18 06:56:06 +0000

    Direct association between users and recommendations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Direct associations
 
+  has_many   :recommendations,
+             :foreign_key => "sender_id",
+             :dependent => :nullify
+
   has_one    :sign_up,
              :foreign_key => "kwestee_id",
              :dependent => :destroy
