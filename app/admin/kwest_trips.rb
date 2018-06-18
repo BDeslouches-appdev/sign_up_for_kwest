b93e52e99c8b74761592ad0c98481a4213330951
Raghu Betina <raghu@firstdraft.com>
2018-06-18 06:56:31 +0000

    Whitelist columns in KwestTrip for ActiveAdmin

--- app/admin/kwest_trips.rb
+++ app/admin/kwest_trips.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register KwestTrip do
+
+ permit_params :trip_name, :region, :is_short
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
