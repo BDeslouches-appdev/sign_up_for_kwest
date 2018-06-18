60472365572f4a12ce307e5a412b666cc63c49da
Raghu Betina <raghu@firstdraft.com>
2018-06-18 06:56:10 +0000

    Direct association between kwest_trips and sign_ups

--- app/models/kwest_trip.rb
+++ app/models/kwest_trip.rb
@@ -1,6 +1,10 @@
 class KwestTrip < ApplicationRecord
   # Direct associations
 
+  has_many   :sign_ups,
+             :foreign_key => "trip_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
